719e7ff9dd847203ae9526dc7ddfbae6c5212d5d
Odixi <vz4ev4r@gmail.com>
2020-02-01 10:45:05 +0200

    ShipGenerator works to m´some extent now

---
+++ Assets/Scripts/SpaceShips/PartPlacingIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartPlacingIndicator : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    public void SetAllowedState(bool allow)
+    {
+        spriteRenderer.color = allow ? Color.green : Color.red;
+    }
+}
