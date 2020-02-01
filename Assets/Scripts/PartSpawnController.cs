8b9a0c61e541b73b953a6b237dbfacff48681fb1
Odixi <vz4ev4r@gmail.com>
2020-02-01 22:58:53 +0200

    Dynamic -> Static

---
+++ Assets/Scripts/PartSpawnController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartSpawnController : MonoBehaviour
+{
+    public float partSize = 1;
+    public GameObject[] parts;
+    public int numberOfParts = 30;
+
+    private void Start()
+    {
+        for(int i = 0; i < numberOfParts; i++)
+        {
+            var point = GetSpawnPoint();
+            var obj = Instantiate(parts[(int)(Random.value * parts.Length)]);
+            obj.transform.position = point;
+        }
+    }
+
+    public Vector2 GetSpawnPoint()
+    {
+        while (true)
+        {
+            var viewportPoint = new Vector2(Random.value, Random.value);
+            var ray = Camera.main.ViewportPointToRay(viewportPoint);
+            var raycastRes = Physics2D.BoxCast(ray.origin, Vector2.one * partSize, 0, ray.direction);
+            if (raycastRes.collider == null)
+            {
+                return Camera.main.ViewportToWorldPoint(viewportPoint);
+            }
+        }
+    }
+
+}
