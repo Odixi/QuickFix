e8bcdbe53fbfad9bf6a6f37202b85ed73a99c88d
Vaintti <antti.jk.vainikka@gmail.com>
2020-02-01 21:14:34 +0200

    Jump through

---
+++ Assets/Scripts/CollisionDisabler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDisabler : MonoBehaviour
+{
+    public int PlayerNumber = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        JumpThrough script = collision.GetComponent<JumpThrough>();
+        if (script == null) return;
+        if (PlayerNumber == 1) script.player1CollisionDisablerIn = true;
+        else script.player2CollisionDisablerIn = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        JumpThrough script = collision.GetComponent<JumpThrough>();
+        if (script == null) return;
+        if (PlayerNumber == 1) script.player1CollisionDisablerIn = false;
+        else script.player2CollisionDisablerIn = false;
+    }
+}
---
+++ Assets/Scripts/JumpThrough.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpThrough : MonoBehaviour
+{
+    public bool player1CollisionDisablerIn = false;
+    public bool player1CollisionEnablerIn = false;
+    public bool player2CollisionDisablerIn = false;
+    public bool player2CollisionEnablerIn = false;
+
+    private void Update()
+    {
+        if (player1CollisionDisablerIn && player2CollisionDisablerIn) gameObject.layer = 10;
+        else if (player1CollisionDisablerIn) gameObject.layer = 8;
+        else if (player2CollisionDisablerIn) gameObject.layer = 9;
+        else if (player1CollisionEnablerIn || player2CollisionEnablerIn) gameObject.layer = 0;
+    }
+}
