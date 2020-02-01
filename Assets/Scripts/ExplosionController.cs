1a3356c53ff16a0a5b225f38b30fa6275be91451
Odixi <vz4ev4r@gmail.com>
2020-02-01 18:26:37 +0200

    Ships can break now

---
+++ Assets/Scripts/ExplosionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    public ParticleSystem explosionParticleSystem;
+
+    private float timeAlive = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAlive += Time.deltaTime;
+        if (timeAlive > explosionParticleSystem.startLifetime + explosionParticleSystem.duration)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
